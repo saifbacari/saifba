e9af46581d8850614bc10a339e1074354d663049
Saifoullah Bacari <saifbacari@gmail.com>
2023-07-25 12:05:02 +0200

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
+  const links = [
+    {name:'1. About',url:''},
+    {name:'2. Experience',url:''},
+    {name:'3. Work',url:''},
+    {name:'4. Contact',url:''},
+    {name:'Resume',url:''},
+  ];
   return (
     <div className="App">
-        <Navbar />
+        <Navbar links={links}/>
     </div>
   )
 }
