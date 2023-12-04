093565001b60f7f8b3e4df36d6751af57609f783
Saïf <53183335+saifbacari@users.noreply.github.com>
2023-12-04 11:56:26 +0100

    analytics vercel adding

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import * as ReactDOM from "react-dom/client"
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import { BrowserRouter } from "react-router-dom"
+import { Analytics } from "@vercel/analytics/react";
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>,
 )
